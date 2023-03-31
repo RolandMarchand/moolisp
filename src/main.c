628f3bef1328630bc7cc0cdb96b14f9c0d9ef1f2
Roland Marchand <roland.marchand@protonmail.com>
2023-03-31 04:07:13 -0400

    Integrated the context with the front end

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
-#include "lexer.h"
-#include "repl.h"
 #include "common.h"
-#include "var.h"
+#include "config.h"
 #include "context.h"
+#include "lexer.h"
+#include "main.h"
+#include "repl.h"
+#include "var.h"
 
 tgc_t gc;
 
@@ -10,9 +12,41 @@ struct var eval(struct var expr);
 struct var progn(struct var list);
 struct var unless(struct var test, struct var then, struct var otherwise);
 
+int _main(int argc, char **argv);
+
 int main(int argc, char **argv)
 {
+	volatile void *dummy;
+	tgc_start(&gc, &dummy);
+	int ret = _main(argc, argv);
+	tgc_stop(&gc);
+	return ret;
+}
 
+int _main(int argc, char **argv)
+{
+	struct context context = get_context(argc, argv);
+	if (context.error) {
+		if (context.error_arg) {
+			fprintf(stderr, "error: %s '%s'\n",
+				context.error, context.error_arg);
+			return EXIT_FAILURE;
+		}
+		fprintf(stderr, "error: %s\n", context.error);
+		return EXIT_FAILURE;
+	}
+	if (context.license) {
+		printf(LICENSE);
+		return EXIT_SUCCESS;
+	}
+	if (context.version) {
+		printf("Moolisp version: %d.%d.%d\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+		return EXIT_SUCCESS;
+	}
+	if (context.repl) {
+		repl();
+	}
+	return EXIT_SUCCESS;
 }
 
 struct var eval(struct var expr)
