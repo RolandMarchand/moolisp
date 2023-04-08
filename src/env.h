fb0b25b85226142985b12b24d00601cf14dacd9a
Roland Marchand <roland.marchand@protonmail.com>
2023-04-08 12:09:54 -0400

    Removed obsolete `env_set_c_fn()`

--- src/env.h
+++ src/env.h
@@ -10,7 +10,5 @@ struct env {
 
 struct env *env_make(struct env *outer, struct var *binds, struct var *exprs);
 void env_set(struct env *current, struct var *symbol, struct var *value);
-void env_set_C_fn(struct env *current, char* symbol_name,
-			 struct var *(*fn)(struct var *));
 struct var *env_get(struct env *current, struct var *symbol);
 struct env *env_find(struct env *current, struct var *symbol);
